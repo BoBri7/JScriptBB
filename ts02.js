cd1e593a24ee3a3bf10abb87a9bdd406ad4c0127
Bogdan <43040397+BoBri7@users.noreply.github.com>
2018-11-06 12:22:48 +0100

    Update ts02.js

--- ts02.js
+++ ts02.js
@@ -1,12 +1,8 @@
- pon., 29. okt. 2018 
-## 
-
-1. ### ****_~~~~ _ ** ** 
-1. 555
-``
-```
-
-```
-` ` 
-
-_Sent from my "HUAWEI MediaPad M5 10.8""" using [FastHub](https://play.google.com/store/apps/details?id=com.fastaccess.github)_:heart_eyes:
+// pon., 29. okt. 2018 
+// Reshad94/MDB Javascript functions for Memento Database scripts
+function pf(n)     {return parseFloat(n);}
+function pi(n)      {return parseInt(n);}
+function ps(n)     {return n.toString();}
+function tp(n)      {return typeof (n);} //?????  ★
+function tf(n, c)   {return n.toFixed(c);}
+// _Sent from my "HUAWEI MediaPad M5 10.8""" using [FastHub](https://play.google.com/store/apps/details?id=com.fastaccess.github)_:heart_eyes:
