ef423c7be8ae0e87f2d034c29a4a91cf9a703c20
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-07-29 11:33:16 +0200

    Update ts02.js

--- ts02.js
+++ ts02.js
@@ -6,3 +6,14 @@ function ps(n)     {return n.toString();}
 function tp(n)      {return typeof (n);} //?????  ★
 function tf(n, c)   {return n.toFixed(c);}
 // _Sent from my "HUAWEI MediaPad M5 10.8""" using [FastHub](https://play.google.com/store/apps/details?id=com.fastaccess.github)_:heart_eyes:
+// upd pon., 29. jul. 2019 
+function f(x,l)  {return (" ".repeat(l)+x).slice(-l)}
+function fn(x,d) {return x.toFixed(d)}
+function fp(n)   {if (n>=0) n="+"+n;  return n }
+function mf(e,f,ff) {return moment(e.field(f)).format(ff)}
+//
+function sys(x)  {s=system().os;
+  return x==0?s:x==1?s.slice(8,9): "\n"+ 
+  "   sys(0=OS, 1=os#, /=help" +"\n"+ 
+  "   srt(FN$,/=dsc 1=asc" +"\n" }
+//
