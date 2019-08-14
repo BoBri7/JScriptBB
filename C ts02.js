c035e28c3bae67e2b2a3d0bba87c1ec856d4a9fb
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-14 13:20:47 +0200

    Update C ts02.js

--- C ts02.js
+++ C ts02.js
@@ -8,18 +8,12 @@ function tf(n,d)   {return n.toFixed(d);}
 // _Sent from my "HUAWEI MediaPad M5 10.8""" using [FastHub](https://play.google.com/store/apps/details?id=com.fastaccess.github)_:heart_eyes:
 // upd pon., 29. jul. 2019 
 function f(x,l)  {return (" ".repeat(l)+x).slice(-l)}
-function fn(x,d) {return x.toFixed(d)}
+function fn(x,d) {return x.toFixed(d)}  => round ★★★
 //function fp(n)   { n==null? return "" : if (n>=0) n="+"+n;  return n }
 function fp(n) { if(n==null) n = ""
  if (n != "" && n>=0) n="+★"+n
  return n }
 
-function emf(e,f,ff) {return moment(e.field(f)).format(ff)}
-function mf(f,ff)    {return moment(  field(f)).format(ff)}
-// SYS return OS # or $
-function sys(x)  {s=system().os;
-  return x==0?s:x==1?s.slice(8,9): "\n"+ 
-  "   sys(0=OS, 1=os#, /=help" +"\n"+ 
-  "   srt(FN$,/=dsc 1=asc" +"\n" }
+
 //
 function sign(x) { return x>0?1:x<0?-1:0 }
