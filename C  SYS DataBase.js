bfc6ecde611ffb1452834f5eca64711eadd51f32
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-14 14:53:21 +0200

    Update C  SYS DataBase.js

--- C  SYS DataBase.js
+++ C  SYS DataBase.js
@@ -3,15 +3,17 @@ function db(ns) { return lib(ns)} // name.$ /null
 function dbe(ns) { return db(ns).entries() }
 // dbs DBSort ★★★
 
-
+/*
 
 
 //★ polja
 function F(f)        {return field(f) }   
-function ef(f,E)     { if(E==null) E=e ;return E.field(f) } //★ E~druga baza ★
+function ef(f,E)     { if(E==null) E=e ;
+  return E.field(f) } //★ E~druga baza ★
 function mf(x,f)     {if(f==null) f="<YY.MM.DD.HH.mm"
-    return moment(x).format(ff) }
+  return moment(x).format(ff) }
 
 //★★★★ SYS return OS # or $ ★★★★
 function sys(x)  { s=system().os;
   return x==0?s:x==1?s.slice(8,9): "sys(#) 0=OS, 1=os#, /=help" }
+*/
