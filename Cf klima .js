eb8fddaf9d2cf52e3a1bcbb51c503774f8545878
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-18 22:09:52 +0200

    Update Cf klima .js

--- Cf klima .js
+++ Cf klima .js
@@ -5,3 +5,5 @@ function v(v,d,e) {  if ( v == null ) return "  ~"
   else { return Number(v).toFixed(d) + e   }}
 function i(x,n,p) { if (p==1 && x >0 ) x="+"+x
   return " "+(" ".repeat(n)+x).slice(-n+1)}
+function ie(x,n,e) { // if (p==1 && x >0 ) x="+"+x
+  return " "+(" ".repeat(n)+x+e).slice(-n+1)}
