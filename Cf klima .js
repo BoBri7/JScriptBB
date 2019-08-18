411b1651eb2e31b90789a5e5bb3b4e888ecca37f
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-18 23:19:31 +0200

    Update Cf klima .js

--- Cf klima .js
+++ Cf klima .js
@@ -1,9 +1,15 @@
-function tf(f,e) {n= e==null?field("Temp."+f):e.field("Temp."+f)
-  if (n!=null) {n+= e==null?field("Temp.d"+f):e.field("Temp.d"+f)}
+function tf(f,e) {if( e==null) {
+  n=field("Temp."+f)
+  if (n!=null)  n+= field("Temp.d"+f)/10 }
+  else { n=e.field("Temp."+f)
+  if (n!=null) n+=e.field("Temp.d"+f)/10}
   return n}
+
 function v(v,d,e) {  if ( v == null ) return "~"
   else { return Number(v).toFixed(d) + e   }}
+
 function i(x,n,p) { if (p==1 && x >0 ) x="+"+x
   return " "+(" ".repeat(n)+x).slice(-n+1)}
+
 function ie(x,n,e) { // if (p==1 && x >0 ) x="+"+x
   return " "+(" ".repeat(n)+x+e).slice(-n+1)}
