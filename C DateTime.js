249ea37de5a3c7a80e9c6ec4c3c5a3e8e9602f01
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-21 10:55:49 +0200

    Update C DateTime.js

--- C DateTime.js
+++ C DateTime.js
@@ -13,14 +13,16 @@ function df(x) { // if(x ==null) x =0
 function bod(d) { return mbe(d,0) }
 function bom(d) { return mbe(d,1) }
 function boy(d) { return mbe(d,2) }
-function eod(d) { return mbe(d,-0) }
+function eod(d) { return mbe(d,"day") }//★★★??? 
 function eom(d) { return mbe(d,-1) }
 function eoy(d) { return mbe(d,-2) } //11
 function dmyh(x) { s="day,month,year,hour,minute,week"; return x==null?s: s.split(",")[Math.abs(x)]}
 function mbe(d,x){ 
   d=moment(d==null ? new Date() : d) ; 
-  e=(x>0);  x=dmyh(x) ;
-  return e==true ? d.startOf(x) : d.endOf(x)  }
+  if(isStr(x)) return d.endOf(x)
+  else {
+    e=(x>0);  x=dmyh(x) ;
+  return e==true ? d.startOf(x) : d.endOf(x)  }}
 function nwd(a) { if(a==null) return new Date() 
   else { a=a.split("."); 
   return new Date(20+a[0],a[1]-1,a[2],a[3],a[4],0,0)}}
