3ad4c7a5c1e3fff3988a125dbc38d0073af8a7c7
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-21 19:05:33 +0200

    Update C MunPh.js

--- C MunPh.js
+++ C MunPh.js
@@ -4,6 +4,7 @@ function MPh(d) { md=29.5344
   M = "191226s,190702m,190106m,0"
   m ="191126,191028,190928,190830,190801,190702,"
   m+="190603,190505,190405,190306,190204,100115,010124"
+                 // preveri ★★★★
   f=m.split(",").find( function(e) {
     return e <= moment(d).format("YYMMDD") })
   n= new Date(20+f.slice(0,2),f.slice(2,4)-1,f.slice(-2))
