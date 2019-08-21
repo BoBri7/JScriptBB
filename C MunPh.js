1d51495c8b8c111a753ab795c08fadb3b4eca829
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-21 16:52:45 +0200

    Update C MunPh.js

--- C MunPh.js
+++ C MunPh.js
@@ -1,6 +1,6 @@
 // Moon Phases
 // izračuna dan v lunini fazi
-function MPh(d) { md=29.305
+function MPh(d) { md=29.5344
   M = "191226s,190702m,190106m,0"
   m ="191126,191028,190928,190830,190801,190702,"
   m+="190603,190505,190405,190306,190204,0"
@@ -11,7 +11,7 @@ function MPh(d) { md=29.305
   return  r -Math.floor(r/md) *md
 }
 // vrne sliko faze iz dneva faze
-function MPs(d) {m=29.305;f="O ) D (D ★ (C C (".split(" ")
+function MPs(d) {m=29.5344;f="O ) D (D ★ (C C (".split(" ")
   d = (d/m*(f.length-1)).toFixed(0)
   return f[d]
 }
