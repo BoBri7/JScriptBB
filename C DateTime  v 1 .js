1fc5983ddba0c0445e1ab9592efefdf454a8ab64
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-27 15:15:41 +0200

    Update and rename C DateTime.js to C DateTime  v 1 .js
     V1

--- C DateTime  v 1 .js
+++ C DateTime  v 1 .js
@@ -1,6 +1,25 @@
+function dt() { S="\n ★★★★★★ \n ★ Date Time funct ★"
+  S+= "\n ★ Ds(d/now,N/2) => Left-N(slo ime dneva) "
+  S+= "\n ★ Now(N) => now()+Ndays"
+  return  S + "\n ★★★★★★★"
+}
+function Ds(DT,NN) { try { 
+  if ( isNaN(NN) ) NN=2 ; if (isNaN(DT)) DT = Now()
+  Dd = moment(DT).format("d")
+  Sd ="nedelja.ponedeljek.torek.sreda."
+  Sd+="četrtek.petek.sobota.nedelja"
+  return Sd.split(".")[Dd].slice(0,NN)//+" >"+DT+"<"
+} catch (er) {er} 
+}
+function Now(X) {
+  return new Date(moment(new Date()).add(X,"days")) 
+} function now(X) { return Now(X) }
+
+/* ★★★★ CANDIDATI ★★★
+
 function tp(n) {return typeof (n) }
 // date moment functions
-function now() {return d=new Date() }
+
 /// 
 function mf(d,f) { if(d==null) d=new Date(); if(f==null) f="DD.MM.YY HH:mm"; 
   return moment(d).format(f) }
@@ -28,3 +47,4 @@ function nwd(a) { if(a==null) return new Date()
   return new Date(20+a[0],a[1]-1,a[2],a[3],a[4],0,0)}}
 function mad(d,n,x) { if(x==null)x=0; if(n==null)n=1;
   return moment(d).add(n,dmyh(x)) } //+"s") }
+★★★ END date f time Esen ★★★ */
