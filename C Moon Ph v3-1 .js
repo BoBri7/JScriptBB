16c7b93816614ae3ef7afe3ae30f4cb199f110de
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-08-27 12:58:54 +0200

    Update and rename C Moon Ph v3 .js to C Moon Ph v3-1 .js
    BugFix

--- C Moon Ph v3-1 .js
+++ C Moon Ph v3-1 .js
@@ -1,5 +1,5 @@
 Vn ="★ Ver 3 tor.27.avg.19 11:26 ★"
-function MP(D,X) { F=29.5344; d1 = "  •  " d2 = " • "
+function MP(D,X) { F=29.5344; d1 = "  •  " ; d2 = " • "
   function ii(S,I){ return S.slice(I*2,I*2+2) } //★inFun★
 
   MF=" × . ).D.(D.O.(C.C.( . x ".split(".") 
