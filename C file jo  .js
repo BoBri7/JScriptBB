279bc476c5a114f3f384239ef0ace85ac98bc7fd
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-02 18:08:29 +0200

    Create C file jo  .js
    File IO functions

---
+++ C file jo  .js
@@ -0,0 +1,71 @@
+function Fo(Fn) { return file(Fn) } 
+function Fw(Fn,txt) {
+  Fn.writeLine(txt); }
+function Fc(Fn) { Fn.close() }
+
+function Fr(Fn) { return Fn.readLines() }  
+
+
+/*
+function Fw(Fn,txt) {  Fn = file(Fn); 
+  Fn.writeLine(txt);   Fn.close()    }
+function Fr(Fn) { return Fn.readLines() }  
+function FR(Fn) { Fn = file(Fn); 
+  Rl = Fn.readLines(); Fn.close(); 
+  return Rl }  */
+/* / ★★★★★
+Working with files
+With scripts, you can read or write files located in the device's internal memory or on the SD card. All file operations are performed by the File object, which is obtained via a global function called file().
+
+To work with the files, the library should have read/write file access.
+
+File Global Functions
+file(name)
+Open a file for read or write operations. If the file with the specified name does not exist yet, it will be created.
+
+Argument
+name
+The name and the full path to the file. For example, if the file is located on the SD card, the path should be something like /sdcard/example.txt.
+Result
+File object
+
+Object File
+This object is returned by the global function file() and provides access to the requested file. After reading or writing, the file should be closed using the method close().
+
+File Methods
+close()
+Close the file. It can subsequently be reopened.
+getLineNumber()
+Get the current line number
+Returns
+The line number, or position, in the file
+readAll()
+Reads all lines of the file, and then closes the file
+Returns
+Array containing the lines of the file
+readChar()
+Reads the next character from the file stream
+Returns
+The character
+readLine()
+Reads the next line from the file stream
+Returns
+The line
+readLines()
+Reads the remaining lines from the file stream
+Returns
+Array containing the remaining lines of the file
+write(text)
+Write string(s). Take a variable number of arguments, converts each argument to a string, and writes that string to the file stream.
+writeLine(text)
+Write strings and a newline to the file stream
+File Properties
+exists
+true — if and only if the file exists; false otherwise
+length
+The length, in bytes, of the file, or 0L if the file does not exist.
+
+Files Examples
+See Trigger Examples#Files Examples. 
+
+*/ ★★★★★★
