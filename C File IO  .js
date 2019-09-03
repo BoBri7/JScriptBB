7571a2810c3e290c15902f40862d8d7ce7983e62
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-03 07:38:53 +0200

    Update C File IO  .js
    Aded f.write text
    Minor ch.

--- C File IO  .js
+++ C File IO  .js
@@ -1,22 +1,24 @@
 function Fo(Fn)     { return file(Fn)    } 
-function Fw(Fn,txt) { Fn.writeLine(txt); }
 function Fc(Fn)     { Fn.close()         }
 function Fx(Fn)     { return Fn.exists() }
 function Fl(Fn)     { return Fn.length() }
 function Fp(Fn)     { return Fn.getLineNumber() }
-                     
-function Frl(Fn) { return Fn.readLine()  }
-function Frl(Fn) { return Fn.readLines() }  
-function Fra(Fnl { return Fn.readAll()   }
-function Frc(Fn) { return Fn.readChar()  }
 
+
+function Fw( Fn,txt) { Fn.write(text)    }
+function Fwl(Fn,txt) { Fn.writeLine(txt) }
+
+function Frc(Fn) { return Fn.readChar()  }
+function Frl(Fn) { return Fn.readLine()  }
+function FrL(Fn) { return Fn.readLines() }  
+function Fra(Fnl { return Fn.readAll()   }
+function FR(Fn) { Fn = file(Fn); 
+  Rl = Fn.readLines(); Fn.close(); return Rl } 
 /*
 function Fw(Fn,txt) {  Fn = file(Fn); 
   Fn.writeLine(txt);   Fn.close()    }
 function Fr(Fn) { return Fn.readLines() }  
-function FR(Fn) { Fn = file(Fn); 
-  Rl = Fn.readLines(); Fn.close(); 
-  return Rl }  */
+ */
 /* / ★★★★★
 Working with files
 With scripts, you can read or write files located in the device's internal memory or on the SD card. All file operations are performed by the File object, which is obtained via a global function called file().
