02412abf19baa722e7c7d348eaa0d30a4bb9119d
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-06 15:54:05 +0200

    Update ErrHandler .js

--- ErrHandler .js
+++ ErrHandler .js
@@ -1,1 +1,14 @@
 // Err Handling
+function LgEr(err,txt) { 
+  txt = "Err:"+err.name+""+err.message+"\n >"+txt )
+  // alert (txt) ;log (txt);
+  return txt
+}  //★★10
+}
+
+/*
+try {  napaka? ) }
+catch(err) { LgEr(err,"txt") }
+finaly { ...  }
+*/
+//★★★★★★★★
