f5379edb313ef2a8e4d8bc7fbc6972b287be436a
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-08 11:03:59 +0200

    Update C U3S.js
    BugFx

--- C U3S.js
+++ C U3S.js
@@ -1,7 +1,7 @@
 function u3s() { 
   s =" ★ U3S funkcije ★\n"
   s+=" Sez(d)  Ret: SezYy(datum) d=null=>akt.sezona\n"
-  sj=" Sz(d)   Ret: Yy(d)/Y+1  \n"
+  s+=" Sz(d)   Ret: Yy(d)/Y+1  \n"
   s+=" Sn(d)   Ret: actSez- Sez(d)  \n"
   s+=" df(d,f) Ret: format(m(d,f=d.m.yy)
   s+=" S(s,x)  Ret:x>0 left(s.x>0) / right(s.x<0) \n"
