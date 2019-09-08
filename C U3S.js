fee2b6f7d00cd82cfc676494e8f0fa77d8df3586
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-08 12:14:37 +0200

    Update C U3S.js

--- C U3S.js
+++ C U3S.js
@@ -25,8 +25,10 @@ function S(S,X) { S=String(S) ;
   return X>0? S.slice(0,X) : S.slice(X) }
 
 // 4 test ==>★★ test gf(fn) ? e.f() ★★
-function gfer(fn) {try { return field(fn) }
-  catch (er) { return e.field(fn) }}
+function gfer(fn) {
+  try { return field(fn) }
+  catch (er) { return e.field(fn) }
+}
 
 function gf(fn,E) {
   return E==null ? field(fn) : E.field(fn)}
