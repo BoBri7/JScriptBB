9426d4b597d51deff5ec59be7f98ef5c4b054ee3
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-08 09:46:38 +0200

    Create C U3S.js
    Funkcije za U3S sezona

---
+++ C U3S.js
@@ -0,0 +1,14 @@
+function u3s() { 
+  s =" ★ U3S funkcije ★\n"
+  s+=" Sez(d) Ret: SezYy(datum) d=null=>akt.sezona\n"
+  s+=" Sn(d)  Ret: actSez- Sez(d)  \ n"
+  s+=" S(s,x) Ret:x>0 left(s.x>0) / right(s.x<0) \n"
+  
+return s + "★ ★ ★\n" }
+  
+function Sez(D) { 
+  return Number(moment(D).add(-188,"day").format("YY")) }
+function Sn(D) {
+  return Sez() - Sez(D) }
+function S(S,X) { return X>0? S.slice(0,X) :S.slice(X) }
+// ★★ END ★★
