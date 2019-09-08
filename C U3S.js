45ab251297e71fbf93efc2671b4760a89a6d98f9
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-08 11:18:46 +0200

    Update C U3S.js
    Corr zamik sezone na 22.8. = 233 dni
    BugFx term.string.

--- C U3S.js
+++ C U3S.js
@@ -1,15 +1,16 @@
 function u3s() { 
   s =" ★ U3S funkcije ★\n"
   s+=" Sez(d)  Ret: SezYy(datum) d=null=>akt.sezona\n"
+  s+="   233 dni zamika =  22.8. \n \n "
   s+=" Sz(d)   Ret: Yy(d)/Y+1  \n"
   s+=" Sn(d)   Ret: actSez- Sez(d)  \n"
-  s+=" df(d,f) Ret: format(m(d,f=d.m.yy)
+  s+=" df(d,f) Ret: format(m(d,f=d.m.yy) \n"
   s+=" S(s,x)  Ret:x>0 left(s.x>0) / right(s.x<0) \n"
   
 return s + "★ ★ ★\n" }
   
 function Sez(D) { if(D==null) D=new Date()
-  D=moment(D).add(-188,"day")
+  D=moment(D).add(-233,"day")
   return Number(D.format("YY"))         }
 
 function Sz(D)   { D = Sez(D) ;
