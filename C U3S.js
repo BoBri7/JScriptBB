c2e8935fad9afa801b0d2a7b81f7803004b6dc1a
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-08 10:14:00 +0200

    Update C U3S.js
    Add f Sz YY/yy

--- C U3S.js
+++ C U3S.js
@@ -1,13 +1,16 @@
 function u3s() { 
   s =" ★ U3S funkcije ★\n"
   s+=" Sez(d) Ret: SezYy(datum) d=null=>akt.sezona\n"
+  sj=" Sz(d)  Ret: Yy(d)/Y+1  \n"
   s+=" Sn(d)  Ret: actSez- Sez(d)  \n"
   s+=" S(s,x) Ret:x>0 left(s.x>0) / right(s.x<0) \n"
   
 return s + "★ ★ ★\n" }
   
-function Sez(D) { 
-  return Number(moment(D).add(-188,"day").format("YY")) }
+function Sez(D) { D=moment(D).add(-188,"day")
+  return Number(D.format("YY"))         }
+funtion Sz(D)   { D = Sez(D) ;
+  return D+"/"+(D+1)                    }
 function Sn(D)  { return Sez() - Sez(D) }
 function S(S,X) { S=String(S) ;
   return X>0? S.slice(0,X) : S.slice(X) }
