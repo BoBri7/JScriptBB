7313175775d937b467bd1457d950f1e8c3edec31
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-10 19:44:44 +0200

    Update C FormatString Len .js
    Fix CL codes

--- C FormatString Len .js
+++ C FormatString Len .js
@@ -18,17 +18,35 @@ function FS( SS, LL) { NN =0 // pad right ★★
  else {     
   for ( ii=0; ii< SS.length; ii++) {
     NN+= CL( SS.charAt(ii) ) }
-  return SS + " ".repeat( Math.max(0,LL-NN/20 )* 2.22 ) 
+ // return SS + " ".repeat( Math.max(0,LL-NN/20 )* 2.22 ) 
+  NN=" ".repeat( Math.max(0,LL-NN/20 )* 2.22 ) 
+  NN=" ".repeat( Math.max(0,LL-NN/20 )* 2.22 ) 
   // +">"+NN.toFixed(1)
+  return LL>0? SS+NN : NN+SS  
+} // ★ 11 */
 }}
 function CL( Ch) { 
   if (Ch==null ) return FSh() ; else {
+/*
  s ="b1bbbbbbk1bb9bbgggggggggggzg1gg999mkqm4dcfk7a9akkkkk"
  s+="kkkkk87ijihvnmnnkjopajmjvpomomllnmvmll9e9fgbjkikjckj"
  s+="98i9vjkkkcibjhqhhhc9cogggggggggggggggmgggggggggggggg"
  s+="ghg99jkpi8lfsggj1sgdjddbkh99dggqrrhnnnnnnxnkkkkaaaan"
  s+="poooopjonnnmllljjjjjjuijjjj9999ljkkkkkkkjiijhkh"
- return parseInt( s.charAt( Ch.charCodeAt(0) ),36 )
+ */
+  s ="b1bbbbbbk1bb9bbgggggggggggzg1gg9" // 1-32
+  s+="99mkqm4ccfk7a9akkkkkkkkkk87ijihv" // 33-64
+  s+="nmnnkjopajmjvpomomllnmvmll9e9fgb" // 65-96
+  s+="jkikjckj98i9vjkkkcibjhqhhhc9cogg" // 97-128
+  s+="gggggggggggggmggggggggggggggghg9" // 129-160
+  s+="9jkpi8lfsggj1sgdjddbkh99dggqrrhn" // 161-192
+  s+="nnnnnxnkkkkaaaanpoooopjonnnnlllj" // 193-224
+  s+="jjjjjuijjjj9999ljkkkkkkkjiijhkhn" // 225-256
+  s+="jnjnjnininininmnlkjkjkjkjkjokoko" // 257-288
+  s+="kokpjpka9a9a9a9a9thj9mijj9j9jbjc" // 289-320
+  s+="j9pjpjpjjokokokokxwmcmcmclililil" // 321-352
+  s+="ilblclbnjnjnjnjnjnjvqlhllhlhlh9l" // 353-384
+  return parseInt( s.charAt( Ch.charCodeAt(0) ),36 )
 }}
 
 //★★★★★
