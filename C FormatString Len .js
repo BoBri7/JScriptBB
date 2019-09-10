c38e89a335771b311d2020efaa3d8094f72f8f65
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-10 17:56:43 +0200

    Update C FormatString Len .js

--- C FormatString Len .js
+++ C FormatString Len .js
@@ -1,4 +1,11 @@
-
+function FSh() { s=" ★★ StringLen / ChrLen ★★\n"
+  s+="FS(s,n) Ret: pad/n>0 right/left S.len  \n"
+  s+="CL(Ch)  Ret: len(20 Ch) \n"
+  s+="Chr(n)  Ret: chr(#) \n"
+  s+="N2C(n,c~36)Ret: 10 > c Sys \n" 
+  s+="C2N(n,c~36)Ret: c > 10sys \n"   
+  return s             
+}
 
 function Chr(N){ return String.fromCharCode(N) }
 function N2C(N,C) { if (C==null) C=36
@@ -7,11 +14,13 @@ function C2N(S,C) { if (C==null) C=36
   return parseInt(S,36) }
 
 function FS( SS, LL) { NN =0 // pad right ★★
+ if ( ss ==null ) return FSh()
+ else {     
   for ( ii=0; ii< SS.length; ii++) {
     NN+= CL( SS.charCodeAt(ii) ) }
   return SS + " ".repeat( Math.max(0,LL-NN/20 )* 2.15 ) 
   // +">"+NN.toFixed(1)
-}
+}}
 function CL( Ch) { 
  s ="b1bbbbbbk1bb9bbgggggggggggzg1gg999mkqm4dcfk7a9akkkkk"
  s+="kkkkk87ijihvnmnnkjopajmjvpomomllnmvmll9e9fgbjkikjckj"
