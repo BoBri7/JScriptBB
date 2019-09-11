3c261fa6dcd07290bbd9feef4e820e15170b7dfc
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-11 19:15:59 +0200

    Update C NUM SYSTEM .js
    bug fix test ver

--- C NUM SYSTEM .js
+++ C NUM SYSTEM .js
@@ -9,40 +9,29 @@
   return s+"\n ★ EndHelp ★\n" 
  } //★ end help ★
 
-function N2C(N,C) { if (C==null) C=36
-  return N.toString(C) }
-function C2N(S,C) { if (C==null) C=36
-  return parseInt(S,C) }
 function int(x) { return Math.floor(x) }
 
-function x2xs() {  Ss ="0123456789" 
-  Ss+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  Ss+="abcdefghijklmnopqrstuvwxyz"            
-  //   123456789o123456789o123456
-  return Ss } //★★★             
-
-function X2N(Nn, Cn) { rv =0  ; Nn=Nn.split(""); // f =1
- if (Cn <37) return C2N(Nn,Cn)
- else {
+function C2N(S,C) { if (C==null) C=36
+  return parseInt(S,C) }
+function X2N(Nn, Cn) { if (Cn <37) return C2N(Nn,Cn)
+ else { rv =0  ; Nn=Nn.split("");  f =1
   if (Cn==null) Cn=62
   Cn = Math.min( Cn,Ss.length)
 //  rv+=Nn
   for (ii=0; ii < Nn.length; ii++ ) {
     x = Nn[Nn.length-ii-1]
-    rv+=  Ss.indexOf(x) //*f
-    //f = f*Cn
+    rv+=  Ss.indexOf(x) *f
+    f = f*Cn
   } 
   return rv
 }}  // ★★★★  
-
-function N2X( Nn, Cn, X) { 
- if (Cn <37) return N2C(Nn,Cn)
- else {
-  if (X==null) { rv = ""
-    Ss = x2xs()
-    if (Cn==null) Cn=62
-    Cn = Math.min( Cn,Ss.length)
-  }
+function N2C(N,C) { if (C==null) C=36
+  return N.toString(C) } //★★★★★
+function N2X( Nn, Cn, X) { if (Cn <37) return N2C(Nn,Cn)
+ else { Ss = x2xs()
+  if (Cn==null) Cn=62
+  Cn = Math.min( Cn,Ss.length)
+  if (X==null) { rv = "" }
   X = null
   N = Math.floor( Nn / (Cn-0) ) ; 
   Nn= Nn-N*(Cn)
@@ -50,6 +39,11 @@ function N2X( Nn, Cn, X) {
   rv+= Ss.slice(Nn,Nn+1)  
   return rv 
 }} // ★★★★
+function x2xs() {  Ss ="0123456789" 
+  Ss+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  Ss+="abcdefghijklmnopqrstuvwxyz"            
+  //   123456789o123456789o123456
+  return Ss } //★★★             
 //>>>>>>>>>>>>>
 /*  ★★ test ★★
 log ( "★★BEGIN★★\n") ; c = 99
