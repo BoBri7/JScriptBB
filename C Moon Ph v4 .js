75aa6946514a6c1a0e964a0b33b43894a7b58c54
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-09-29 21:27:13 +0200

    Update C Moon Ph v4 .js
    Bug & opt fix

--- C Moon Ph v4 .js
+++ C Moon Ph v4 .js
@@ -1,55 +1,64 @@
-function MP(D,X) { dd = " • " 
+function MP(D,X) { //var dd = moment(D).format("DD.MM.YY")
  function ii(S,I){ return S.slice(I*2,I*2+2) } //★inFun★
  var F=29.5344
  var MF=" ×. ).D.(D.O.(C.C.( "//. x " 
  
- var Md="191126,191028,190928,190830,190801,190702,190603"
- Md+=",190505,190405,190306,190204,100115,"
- Md+="010124,000001";
+ var Md="000001,010124,100115,190204,190306,190405,"
+ Md+="190505,190603,190702,190801,190830,190928,"
+ Md+="191028,191126"
 
  var Mrk="191226s,190702m,190106m,0"; 
 
  if(D==0) return F
  if(D==1) return MF
  if(D==2) return Md
- if(D==3) return Mrk
- 
- if(D < 9 ) { 
+ if(D==3) return Mrk 
+ if(D==9) { 
   D = "★★★ MP(datum, op ) ★★★ \n"
   D+= "★★★ Ver 4.1 29. 9. 2019  ★\n"
-  D+= " Mf:[ " +MF.join(dd)+"] "+MF.length +" faz\n"
+  D+= " Mf:[ " +MF+"] "+MF.length +" faz\n"
   D+= " Mf= "+F+" dni \n"
-  D+= "★ op= 0:Faza \n"
-  D+= "★ 1:Faza # 29,5 \n"
-  D+= "★ 2:Faza # 8 \n"
-  D+= "★ 9: datum(DMY) \n"
+  D+= "★ d  0: faza dni \n"
+  D+= "★     1: faze \n"
+  D+= "★     2: datumi mlajev \n"
+  D+= "★     3: datumi mrkov \n"
+  D+= "★     9: help \n"
+
+  D+= "★ op 0/ : Faza \n"
+  D+= "★       1: dan faze /29,5 \n"
+  D+= "★       2: dan faze / 8 \n"
+  D+= "★       3: dan + faza \n"
+  D+= "★       6: datum \n"
+  D+= "★       7: datum mlaja  \n"
+  D+= "★       8: faza dni \n"
+  D+= "★       9: faze \n"
   return D }
+
 if ( D==null || isNaN(D) ) D = new Date() 
 if ( moment(D).format("Y.MM") < "2000.01"){ 
  return D+"= ? "+moment(D).format("Y.M.D < 2000 ? ")}
  
- Md = Md.split(",").sort(function(a,b){return b-a})
+ Md=Md.split(",").sort(function(a,b){return b-a})
  MF=MF.split(".")
- N=Md.find( function(X) { //★sort
-  return X <= moment(D).format("YYMMDD") })
+ N=Md.find( function(X) { 
+      return X < moment(D).format("YYMMDD") })
  M= new Date(20+ii(N,0),ii(N,1)-1,ii(N,2),12,0)
- N= moment(D).add(-1,"day").diff(M,"days") 
+ N= moment(D).add(0,"day").diff(M,"days") +1
  N= N-F*Math.floor(N / F)
  N= N.toFixed(1) 
  NF= Math.round( N / F * (MF.length-1) )
  N= (String.fromCharCode(9).repeat(9)+N).slice(-6) //★★
  Mf= MF[NF]
  switch ( Number(X) ) {
-  case 9 :{ return MF.join(dd); break }
+  case 9 :{ return MF.join(", "); break }
   case 8 :{ return F ; break }
   case 7 :{ return moment(D).format("DD.MM.YY") ; break }
+  case 6 :{ return moment(M).format("DD.MM.YY") ; break }
   
-  case 3 :{ return NF +dd+Mf ; break }
-  case 2 :{ return N ; break }
-  case 1 :{ return NF ; break }
+  case 3 :{ return NF +" = "+Mf ; break }
+  case 2 :{ return N ;         break }
+  case 1 :{ return NF ;        break }
   case 0 :{ return moment(M).format("DD.MM.YY") ; break }
   default: return Mf
  } //★ end sw}
-
 }//★ END functuon MP ★★★ 
-// function Chr(n) {return String.fromCharCode(n)}
