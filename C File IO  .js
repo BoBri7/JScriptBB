636d48444c4631fd6ec6b1f71fc921c35a4c8aca
Bogdan <43040397+BoBri7@users.noreply.github.com>
2019-12-13 18:54:51 +0100

    Update C File IO  .js

--- C File IO  .js
+++ C File IO  .js
@@ -1,14 +1,18 @@
-function io() { return "v 30. 9. 2019 " }
+function io() { return "v 13. 12. 2019  " }
+
 function Fo(Fns,ext)     { if (ext==null) ext=".txt"
   return file("/sdcard/"+Fns +ext)    } 
+
 function Fc(Fno)     { Fno.close()         }
-function Fx(Fns)     { return Fns.exists() } //??★
-function Fl(Fno)     { return Fno.length() }
-function Fp(Fno)     { return Fno.getLineNumber() }
 
 function Fw( Fno,txt) { Fno.write(txt)    }
 function Fwl(Fno,txt) { Fno.writeLine(txt) }
 
+function FW(Fn,txt)     { 
+  let o = "/sdcard/"+Fn)   
+  o.write(txt) 
+  o.close()         
+}
 function Frc(Fno) { return Fno.readChar()  }
 function Frl(Fno) { return Fno.readLine()  }
 function FrL(Fno) { return Fno.readLines() }  
@@ -19,6 +23,11 @@ function FR(Fns) { Fno = file("/sdcard/"+Fns);
   return Rl } 
 function FRa(Fns) { Fno = file("/sdcard/"+Fns); Fno.close(); 
   return Fno.readLines();} 
-//★★★ END FILE IO ★★★
 
-// f = file("/sdcard/myfile.txt"); 
+function Fx(Fns)     { return Fns.exists() } //??★
+function Fl(Fno)     { return Fno.length() }
+function Fp(Fno)     { return Fno.getLineNumber() }
+
+
+//★★★ END FILE IO ★★★
+// f = file("/sdcard/MyData/myfile.txt"); 
