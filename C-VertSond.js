1deb8120bd66c072f96ffa9559ff55162cd73a8c
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-01-26 15:47:11 +0100

    Update C-VertSond.js
    Bug fix

--- C-VertSond.js
+++ C-VertSond.js
@@ -1,10 +1,17 @@
 // izračun % rel.vl. iz temp°C in Rosišča
-
 f=0.05
+function rv(t,r) { 
+function e(x){return Math.exp(x*f)} 
+ t = (t==null || r==null) 
+ ? "~" 
+ : (100*e(t)).toFixed(0) 
+ return ("   "+t).slice(-3)
+}
+/* f=0.05
 function rv(t,r) { t=(r==null)?null:r-t
  function e(x){return Math.exp(x*f)}
  t = (t==null)
   ? "~"
   : (100*e(t)).toFixed(0)
  return "???   "+t).slice(-3)
-}
+} */
