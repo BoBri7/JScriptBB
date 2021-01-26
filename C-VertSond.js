559ce6dbd6b1e1f4e80eba7e4124df631d2b89b7
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-01-26 15:01:22 +0100

    Create C-VertSond.js
    8nit

---
+++ C-VertSond.js
@@ -0,0 +1,9 @@
+// izračun % rel.vl. iz temp°C in Rosišča
+
+f=0.07
+function rv(t,r) { t=(r==null)?null:r-t
+ function e(x){return Math.exp(x*f)}
+ return (t==null)
+  ? "~"
+  : (100*e(t)).toFixed(0)
+}
