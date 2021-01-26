64bcd873996c51a048c4adc45455040eca4750b9
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-01-26 15:18:30 +0100

    Update C-VertSond.js
    Correct format

--- C-VertSond.js
+++ C-VertSond.js
@@ -4,6 +4,6 @@ f=0.07
 function rv(t,r) { t=(r==null)?null:r-t
  function e(x){return Math.exp(x*f)}
  return (t==null)
-  ? "~"
-  : (100*e(t)).toFixed(0)
+  ? " ~"
+  : (" "+(100*e(t)).toFixed(0)).slice(-2)
 }
