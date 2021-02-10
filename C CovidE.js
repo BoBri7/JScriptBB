49cb371ce9bbc520909a85ee2a50e7a617b939b8
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-02-10 12:49:49 +0100

    Update C CovidE.js
    Preb/o comment

--- C CovidE.js
+++ C CovidE.js
@@ -4,4 +4,11 @@ function f(x){x ="    "+field(o+"."+x)
 /* orig
 function f(x){x ="   ____ "+field(o+"."+x)
  return x.slice(-5) }
+ 
+O.  preb.   u.   u/1000
+R.  4142.   2.  0.5
+S 17729. 47.  2.7 
+K 26078. 15.  0.6
+H   9139. 14.  1.5
+Z 16402. 17.  1 .0
 */
