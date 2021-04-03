22cfbc05a9c45196f1db14c9c9cbc0410c1c3f52
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-04-03 17:56:00 +0200

    Update C CovidE.js
    Rewrited

--- C CovidE.js
+++ C CovidE.js
@@ -1,82 +1,80 @@
-// ★  // prebivalci /občinah
-function pr(o) {
- switch (o.toLowerCase()) { 
- case "r" : o= 4142 ;break 
- case "s" : o=17729 ;break
- case "k" : o=26078 ;break
- case "h" : o= 9139 ;break
- case "z" : o=16402 ;break
- default  : o=9999999 } 
- return o 
-}// ★ // vrednost polja
-function f(o,x,s,n){ 
- if(n==null)n=9
- if(s==null)s=""
+function pr(o) { //★ prebivalci /občinah
+ switch (String(o).toLowerCase()) {
+ case "r" : o=   4142 ;break 
+ case "s" : o=  17729 ;break
+ case "k" : o=  26078 ;break
+ case "h" : o=   9139 ;break
+ case "z" : o=  16402 ;break
+ default  : o=2000000 }
+ return o/1000 
+} //★10
+function f(o,x,s,n){//★ vrednost polja
+ if(n==null)n=9;  if(s==null)s=""
  o =field(o+"."+x) 
  if(isNaN(o)||o==null)o="~ "
  if(parseInt(s)>0)o+=x.slice(0,s)
  else o+=s
- return ("     "+o).slice(-n) 
+ return ("    "+o).slice(-n) 
+} //★18
+function F(o,x) { return field(o+"."+x) 
 }
-// ★ izpis cov.# za občino
-function obcn(o){ 
+function p(o,x,f) { if(f==null)f=1
+ x=x*f/pr(o)
+ return x.toFixed(x>9?0:1)
+}//★ 23
+function obcn(o){//★ izp.cov.#/občino
  if(!field("F"))return " ~ ~\n ~ ~"
- var i =f(o,"potr","ok:")
- i+=("   "+(parseInt(i)*100/pr(o)).toFixed(1)).slice(-4)+"%;"
- 
- i+=f(o,"vsi","}") 
- var t=field(o+".vsi")
- if(!(isNaN(o)||o==null)) {
-  i+="" /stp="+ (t/pr(o)).toFixed(0) }
- i+="\n"
- 
- i+=f(o,"novi","n,",7)        //novi/
- i+=f(o,"umrl","u",8)         // umrli
- 
- //i+=f(o,"d.po2","d×",11)    //dni/podvojit
- //if(o=="R")i+=f(o,"tnp","°/t") //nov/teden
- 
- i+=("       R*"+R(o).slice(-9)
- 
+ var i =f(o,"potr","okuž:")
+ i+=("  "+p(o,F(o,"potr"))).slice(-4)
+ i+="%; akt.p="
+ i+=f(o,"vsi","}/tp:",12)
+ i+=p(o,F(o,"vsi"))//+" p"+pr(o)
+
+ i+="\n" //★ 31
+
+ i+=f(o,"novi","n," ) //novi/
+ i+=f(o,"umrl","u," ) // umrli
+ //i+=f(o,"d.po2","d×",11) //dni/podvojit
+ //if(o=="R")i+=f(o,"tnp","°/t")
+ //nov/teden
+ i+=("        R*"+R(o)).slice(-12)
  return i 
-}
-// ★★ case / 1000 prebivalcev
-//c="potr;novi;vsi;d.po2".split(";")
-//cpt(c[0])  // 
-function cpt(c) {
- s =""; o ="RSKHZ".split("")
+}//★40
+function cpt(c) { //★ sum 
+ C="potr;novi;vsi;d.po2".split(";")
+ if(c==null) return C;
+ var s ="", o ="RSKHZ".split("")
  for(i=0;i<o.length;i++){
-  v=field(o[i]+"."+c)
-  if(c.slice(-1)!="2" ) {
-   v=(v*1000/pr(o[i])).toFixed(1) }
-  s+=o[i]+":"+("     "+v).slice(-5)
-  s+=(i==2)?"\n ":i<o.length-1?";   ":" "
- }
-return s 
-}
-function R(o){ 
+ v=field(o[i]+"."+C[c])
+ if(C[c].slice(-1)!="2" ) {
+  v=(v*1000/pr(o[i])).toFixed(1) }
+ s+=o[i]+":"+("★ "+v).slice(-5)
+ s+=(i==2)?"\n ":i<o.length-1?"; ":" "
+ } return s +" "+C[c]
+}//★52
+function R(o){
  function r(o,n){
   var p="d.po2;tpnp".split(";")
   v=field( o+"."+p[n])
   o="d%"[n]
   if(v==null) { return null }
   switch (n) {
-  case 0:v=-50*Math.log(v)+280+0.1*v
-     break
-  case 1:v= 90*Math.exp(0.013*v)+10
-     break
-  } return v
- }  o= avg([r(o,0),r(o,1)])
+  case 0:v=-50*Math.log(v)+280+0.1*v;
+  break
+  case 1:v= 90*Math.exp(0.013*v)+10 ;
+  break }
+ return v 
+ }
+ o= avg([r(o,0),r(o,1)])
  if(o=="0") return "~ "
- else return o.toFixed(r>9?0:1))
-}
-function avg(a){var l=a.length,s=0,j=0
+ else return o.toFixed(o>9?0:1)
+}//★16★68
+function avg(a){var l=a.length,s=0,j=0 
  //if(l==0)return "~"
- for(i=0;i<l;i++){ai=a[i]
+ for(i=0;i<l;i++){ ai=a[i]
   if(!(isNaN(ai)||ai==null)){j++;s+=ai}
- } return j==0?"0":s/j 
-}
-//★★
-
+ }
+ return j==0?"0":s/j 
+}//★★ //★★★★ E N D ★
 
 //★★★★ E N D ★
