afaba07e7d3b0288fc72f1efc2ba707206f07113
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-04-03 15:49:13 +0200

    Update C CovidE.js
    Test 
    /stp
    R*

--- C CovidE.js
+++ C CovidE.js
@@ -23,16 +23,18 @@ function obcn(o){
  if(!field("F"))return " ~ ~\n ~ ~"
  i =f(o,"potr","ok:")
  i+=("   "+(parseInt(i)*100/pr(o)).toFixed(1)).slice(-4)+"%;"
- i+=f(o,"vsi","}\n",12)
+ i+=f(o,"vsi","}  /stp=") 
+ i+=((field(o+".vsi")/pr(o)).toFixed(0)
+ i+="\n")
+ 
  i+=f(o,"novi","n,",7)        //novi/
  i+=f(o,"umrl","u",8)         // umrli
  
  //i+=f(o,"d.po2","d×",11)    //dni/podvojit
  //if(o=="R")i+=f(o,"tnp","°/t") //nov/teden
- //r=R(o)
- //i+=f(r.toFixed(r>9?0:1),"R*")
- //i+="   R*"+r.toFixed(r>9?0:1)
-
+ r=R(o)
+ i+=("       R*"+(r.toFixed(r>9?0:1)).slice(-9)
+ 
  return i +" R*"
 }
 // ★★ case / 1000 prebivalcev
