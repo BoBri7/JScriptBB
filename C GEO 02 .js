fcdf526a5897e50d822ad070aa2ae2e8bed0b2d1
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-05-25 13:58:46 +0200

    Update C GEO 02 .js
    Ver.14

--- C GEO 02 .js
+++ C GEO 02 .js
@@ -67,5 +67,5 @@ function L2ENH(x,h){
 //★★★ END
 h+=l+" ★★★ end help  ★★★ "
 
-function gh(x){return x!=null?h:"ver:105'25•13"}
+function gh(x){return x!=null?h:"ver:105'25•14"}
 //Tue, 25 May 2021 12:53:17  
