f4fa40d8ff0caad877afc9a17b33326b48d98c56
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-23 17:30:54 +0100

    Update C Slider .js

--- C Slider .js
+++ C Slider .js
@@ -3,7 +3,7 @@ function slider(ip,tt,tx){var m=moment(),c=ip>0?1:-1
  function l(x){if(c==1)message(x); else log(x) 
  }
  if(tt==null)  return m
- if(m-tt<55) return tt
+ if(m-tt<888) return tt
  l(tx+" ["+"|".repeat(ip*50*c)+".".repeat(50-ip*50*c)+"]")
  return m
 } //★★★
