753f39b760e813efb0e1f9742e85eee6427b95ce
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-23 16:03:36 +0100

    Create C lg msg timer .js
    Init

---
+++ C lg msg timer .js
@@ -0,0 +1,21 @@
+//T=timer // elapsed miliseconds
+//lg=log /0=begin/1=end /txt
+//msg
+// added GH 23. 12. 2021 
+function T(tx){
+ function m(){return moment()}
+ return tx==null
+  ?m()//.add(-22,"milisec")
+  :"Diff msec:"+m().diff(tx,"milisec")
+} 
+function lg(xx){var tt=moment()
+ tt=tt.format("★H:mm:ss.SSS")
+ log( xx==null || xx==0
+  ?"BGN. "+tt+"\n.\n"
+  :xx==1
+   ?"\n> END. "+tt
+   :xx
+ )
+}
+function msg(m){message(m)}
+//★★ end 
