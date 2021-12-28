bc30a8ac39ae5d7c851ae6d696027865f273d392
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-28 09:33:34 +0100

    Create C . FunctionsStrNum .js

---
+++ C . FunctionsStrNum .js
@@ -0,0 +1,13 @@
+//== splošne $# funkcije
+function ps(s,n){//=pad left/rigth
+ return s=n>0
+ ?s+" .".repeat(mx(1,n-s.length))
+ :" "+". ".repeat(mx(1,-n-s.length))+s
+}
+function mx(a,b){//= maximum
+ return Math.max(a,b)
+}
+function mn(a,b){//= minimum
+ return Math.min(a,b)
+}
+*/
