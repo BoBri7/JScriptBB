adee59d7315f30c221c89539d8c46379a671dc02
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-29 14:05:08 +0100

    Update C . Date format slo .js
    Bfx
    Add min sec msc

--- C . Date format slo .js
+++ C . Date format slo .js
@@ -1,12 +1,12 @@
 // DateFormat(d,fstring)
 function m(x){return moment(x)}
-function df(d,df){
+function df(d,df){var F=""
  function f(x){return d.format(x)}
  if(df==null)df=0
  df=String(df)
  d=moment(d); var F=""
- for(i=0;i<df.length;i++){
-  fi=df.charAt(i)
+ for(var ii=0;ii<df.length;ii++){
+  var fi=df.charAt(ii)
   switch (fi){
   case "0":F+=f("DD.MM.YY");break
   case "1":F+=f("D.M.YY")  ;break
@@ -15,20 +15,20 @@ function df(d,df){
   case "4":F+=dw(d)  ;break
   case "5":F+=dm(d,3);break
   case "6":F+=dm(d)  ;break //<
-  case "7":F+=dm(d)  ;break //<
-  case "8":F+=dw(d,3)
-             +f(" D.")
-             +dm(d,3)
-             +f(" YY");break
+  case "7":F+=dw(d,2)+f(" d.m.Y");break
+  case "8":F+=dw(d,3)+f(" D.")+dm(d,3)+f(" YY");break
   case "9":F+=f("HH:mm");break
   case "d":F+=f("D") ;break
   case "D":F+=f("DD");break
   case "m":F+=f("M") ;break
   case "M":F+=f("MM");break
+  case "y":F+=f("Y") ;break
   case "Y":F+=f("YY");break
-  case "y":F+=f("Y");break
-  case "w":F+=f("w");break
+  case "w":F+=f("w") ;break
   case "W":F+=f("WW");break
+  case "n":F+=f("");break //minute
+  case "s":F+=f("ss");break
+  case "S":F+=f("SSS");break
   default:F+=fi
  }}
  return F
