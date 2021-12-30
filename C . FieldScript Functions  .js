8140605e1d2ac66369b99e76394c83c4a76f122f
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-30 08:33:40 +0100

    Update C . FieldScript Functions  .js
    Add fd()

--- C . FieldScript Functions  .js
+++ C . FieldScript Functions  .js
@@ -1,5 +1,10 @@
 //== C . field Functions  .js
 //== funkcije za js Fields
 function f(x){return field(x)}
+function fd(x,df){x=field(x)
+ return x!=null?x:df!=null?df:"~"
+                  
+                  //"99999"
+}
 // function m(x){return moment(x)}
 //== end ==
