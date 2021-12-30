cec5bc2e5f8aa14ac3726c2d89da7f51fcd7ce02
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-30 07:50:56 +0100

    Create C .  Functs .js

---
+++ C .  Functs .js
@@ -0,0 +1,14 @@
+
+// splošne $# f
+function ps(s,n){//=pad left/rigth
+ return s=n>0
+ ?s+" .".repeat(mx(1,n-s.length))
+ :" "+". ".repeat(mx(1,-n-s.length))+s
+// return s
+}
+function mx(a,b){//= maximum
+ return Math.max(a,b)
+}
+function mn(a,b){//= minimum
+ return Math.min(a,b)
+}
