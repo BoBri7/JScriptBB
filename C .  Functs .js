4b8564ed1377cb47aa8dc7e6300866855ce4c45b
Bogdan <43040397+BoBri7@users.noreply.github.com>
2021-12-30 08:20:56 +0100

    Update C .  Functs .js
    Ad f slice

--- C .  Functs .js
+++ C .  Functs .js
@@ -1,5 +1,9 @@
 
 //= splošne $# f
+function sl(x,z,n){ //= slice
+ if(n==null)return x.slice(z)
+ return x.slice(z,z+n)
+}
 function ps(s,n){//=pad left/rigth
  return s=n>0
  ?s+" .".repeat(mx(1,n-s.length))
