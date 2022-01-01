de277b5bc2efeb9c0d1676a9ccb4615785d7e5e8
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-01-01 10:30:58 +0100

    Update C . FieldScript Functions  .js
    Bfx

--- C . FieldScript Functions  .js
+++ C . FieldScript Functions  .js
@@ -1,15 +1,14 @@
 //== C . field Functions  .js
 //== funkcije za js Fields
 function h(){var r="fieldFunctions"
- r+=" f(x)field(x)"
- r+=" fd(x)field(x)/dflt/~" 
+ r+="\n f(x)field(x)"
+ r+="\n fd(x)field(x)/dflt/~" 
  return r
 }             
 function f(x){return field(x)}
 function fd(x,df){x=field(x)
  return x!=null?x:df!=null?df:"~"
-                  
-                  //"99999"
+  //"99999"
 }
-// function m(x){return moment(x)}
+
 //== end ==
