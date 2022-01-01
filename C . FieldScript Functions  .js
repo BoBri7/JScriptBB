f733d6810597041ad3e4512ea35380d625e83811
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-01-01 09:11:42 +0100

    Update C . FieldScript Functions  .js
    Add h()

--- C . FieldScript Functions  .js
+++ C . FieldScript Functions  .js
@@ -1,5 +1,10 @@
 //== C . field Functions  .js
 //== funkcije za js Fields
+function h(){var r="fieldFunctions"
+ r+=" f(x)field(x)"
+ r+=" fd(x)field(x)/dflt/~" 
+ return r
+}             
 function f(x){return field(x)}
 function fd(x,df){x=field(x)
  return x!=null?x:df!=null?df:"~"
