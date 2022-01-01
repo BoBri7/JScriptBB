a7127b0b4666d94c1a0450d9ac0be4cfe165c2a1
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-01-01 09:22:03 +0100

    Update C . ActnTrigScript Functions  .js
    Add h

--- C . ActnTrigScript Functions  .js
+++ C . ActnTrigScript Functions  .js
@@ -1,5 +1,10 @@
 //== C . field Functions  .js
 //== funkcije za js Fields
+function h(){var r="ActFunctions"
+ r+=" f(x)e.field(x)"
+ r+=" //fd(x)field(x)/dflt/~" 
+ return r
+}            
 function f(x){return e.field(x)}
 //function m(x){return moment(x)}
 //== end ==
