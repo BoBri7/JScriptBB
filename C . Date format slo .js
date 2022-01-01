12ef15f46eeab03bddfcf99e8c26875c1602c380
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-01-01 09:48:17 +0100

    Update C . Date format slo .js
    Add h

--- C . Date format slo .js
+++ C . Date format slo .js
@@ -1,4 +1,18 @@
 // DateFormat(d,fstring)
+function h(){var r=" DateFormat"
+ r+="m(x){moment(x)"
+ r+="df(d,f){" 
+ r+=" 0=DD.MM.YY 1=D.M.YY"
+ r+=" 2=dd 3=ddd 4 ddd"
+ r+=" 5=MM ?x6=MMM"
+ r+=" 6=dow "
+ r+=" 7=dd D.M.YY 8= ddd DD.MM.YY" 
+ r+=" 9=HH:mm"
+ r+=" dD mM yY wW hH n sS x=doy"  
+ r+="doy(y)  boy(d)  bod(d) dm dw 
+ r+="u1(x){1st uCase"  
+ return r
+}            
 function m(x){return moment(x)}
 function df(dx,df){
  function f(xx){return dx.format(xx)}
