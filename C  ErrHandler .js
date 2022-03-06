71e1050c9795a206c94644271bfd261cfd3a0049
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-03-06 07:52:32 +0100

    Update C  ErrHandler .js
    BgFx

--- C  ErrHandler .js
+++ C  ErrHandler .js
@@ -1,4 +1,13 @@
 // Err Handling
+function LgEr(er,t){
+ var l=er.line//number 
+ var e="★Er: "+er.message
+ e+="\n "+er.name
+ e+=l>0?" L="+l:""
+ e+=t==null?"<":" - "+t
+ return e
+} //=8
+/* pr.v.
 function LgEr(er,t) { 
   t ="Err:"+er.name+""+er.message
   t+="\n >L="+er.linenumbe +" "+ t )
@@ -6,8 +15,9 @@ function LgEr(er,t) {
   return t
 }  //★★10
 }
-
-/*
+*/
+/* err sintax
+>rry { throw "myErrror"}
 try {  ★ napaka? ★ }
 catch(err) { LgEr(err,"txt") }
 finaly { ★ close ★  }
