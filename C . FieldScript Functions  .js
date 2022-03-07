17d9cb8168c9ca7e67671adc9e162fef40d15e3d
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-03-07 15:41:26 +0100

    Update C . FieldScript Functions  .js
    Added f 4 AIR2set

--- C . FieldScript Functions  .js
+++ C . FieldScript Functions  .js
@@ -14,4 +14,23 @@ function pr(x){
  var e=entry().prev()
  return e==null?"~":e.field(x)
 }
+function L(x){return x.length}
+function sj(x,d){
+ x=x.split(d==null?" ":d)
+ return d+x.join(d)+d
+}
+function v(x,a,f){x=x.split(" ")
+ var s=">"
+ for(var i=0;i<x.length;i++){
+  var xi=x[i].trim()+"."+a
+  var si=" ".repeat(22)+(f>0
+         ?pr(xi)
+         :field(xi))
+  s+=si.slice(-xi.length)+d
+ } return s
+}
+
+
+
+
 //== end ==
