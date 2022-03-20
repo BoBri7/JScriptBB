1e2e3c4a0c046c030ce551a4862f174f37af628b
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-03-20 15:20:08 +0100

    Update B  TOŠL  .js
    Corr  V()

--- B  TOŠL  .js
+++ B  TOŠL  .js
@@ -1,11 +1,20 @@
 function m(x){return moment(x)}
 function f(x,e){return e==null?field(x):e.field(x) } 
 function P(e){return e==null?entry().prev():e.prev()}
-function V(e,x){if(typeof(e)=="string"){x=e;e=entry()}
+
+function V(e,y,n){
+ if(typeof(e)!="object"){n=y;y=e;e=entry()}
+ if(y==null){y=" ★ BRISANO ★ ";n=""}
+ if(n==null)n=""
+ return (f("VOID",e)?y:n)
+}
+/*xxxxxx
+function Vx(e,x){if(typeof(e)=="string"){x=e;e=entry()}
  return (f("VOID",e)
   ?x==null
    ?" ★ BRISANO ★ "
    :(typeof(x)=="string" ?x :true)
   :( typeof(x)=="string" ?x :x==0?"":false))
 }
+*/
 //★★★★★★★
