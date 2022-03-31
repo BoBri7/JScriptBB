436b7abd3973063952efbd4dd12b0a5f50d59f96
Bogdan <43040397+BoBri7@users.noreply.github.com>
2022-03-31 11:08:29 +0200

    Update C .  Functs .js

--- C .  Functs .js
+++ C .  Functs .js
@@ -1,25 +1,33 @@
 
-//= splošne $# f
+/* = splošne $# f
+★chk sysf.padEnd / $.padStart(L,$)
+★ ne deluje
+*/
+//
 function h(){var r="Functions"
  r+=" sl(x,z,n){ slice"
  r+=" ps(s,n){   pad -l/+r" 
  r+=" mx(a,b){ max"
  r+=" mn(a,b){ min"           
  return r
-}             
-function sl(x,z,n){ //= slice
+}    
+//= slice
+function sl(x,z,n){ 
  if(n==null)return x.slice(z)
  return x.slice(z,z+n)
 }
-function ps(s,n){//=pad left/rigth
+//=pad left/rigth
+function ps(s,n){
  return s=n>0
  ?s+" .".repeat(mx(1,n-s.length))
  :" "+". ".repeat(mx(1,-n-s.length))+s
 }
-function mx(a,b){//= maximum
+//= maximum
+function mx(a,b){
  return Math.max(a,b)
 }
-function mn(a,b){//= minimum
+//= minimum
+function mn(a,b){
  return Math.min(a,b)
 }
 //== end ==
