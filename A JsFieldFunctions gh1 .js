88993e1fd1f8635bd83ed3010b0856acf99f5601
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-06-07 08:09:31 +0200

    Update A JsFieldFunctions gh1 .js
    Upd prl

--- A JsFieldFunctions gh1 .js
+++ A JsFieldFunctions gh1 .js
@@ -35,7 +35,7 @@ function round(xv,n){ n=n!=null ?n :1 //def=1
 // padLeftRight
 function prl(s,n,p){ if(p==null)p=" "
  n=n!=null ?n :15*(typeof(v)!="string"?-1:1)
- if(n>0 && n<1) return p.repeat(1/n)+s
+ if(n>0 && n<=1) return p.repeat(1/n)+s
  //s= (n>0 ?s:"")+p.repeat(Math.abs(n))+(n>0 ?"" :s)
  p=""+p.repeat(Math.abs(n))
  return n>0
