733d09aa6b028bf47100296cec19d7260c75d90d
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-06-08 07:41:10 +0200

    Create A ListsText2Lines .js
    1.c

---
+++ A ListsText2Lines .js
@@ -0,0 +1,31 @@
+/*
+* t2l(txt, hed, ll=25, HedDl=":")
+   = prepiše text v vrstice dolžine ll
+* l2l(lfn, hd, ll)
+   = prepiše ListField v vrstice 
+*/
+function t2l(txt, hd, ll, dl){ if(ll==null)ll=25
+  const d=" "
+  var o="", l="t:"+d.repeat(hd-2)
+  if( typeof(hd)=="string" ){
+     l=hd+(dl!=null ?dl:":"); hd=d.repeat(hd.length)
+  }else hd = d.repeat(hd)
+  txt=txt.split(" ")
+  for(var ii=0;ii<txt.length;ii++){ ti=txt[ii]
+   if(  l.length + ti.length /1.7 < ll ) l+= d
+   else { o+= l+"\n" ; l=hd  }
+   l+= ti
+  }
+ return o+l
+}
+
+function l2l(lfn, hd, ll){
+  var o=hd!=null ?hd :lfn+":"
+  lfn=field(lfn)
+  // lfn[1].checked = true
+  for(let ii=0;ii<lfn.length;ii++){ti=lfn[ii]
+    o+="\n "
+    o+=t2l(String(ti), ti.checked ?"[•]" : "[  ]",ll!=null ?ll :25,"" )
+  }
+  return o
+}
